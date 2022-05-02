8cf616e8e3fedb203b0f5a90a732462d1ef18a9b
Herve Desrosiers <hdesrosiers@stingray.com>
2022-05-02 09:48:53 -0400

    adding css

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,5 @@
 const Navbar = () => {
-  return ( 
+  return (
     <nav className="navbar">
       <h1>The Dojo Blog</h1>
       <div className="links">
@@ -7,7 +7,7 @@ const Navbar = () => {
         <a href="/create">New Blog</a>
       </div>
     </nav>
-   );
-}
- 
+  );
+};
+
 export default Navbar;
