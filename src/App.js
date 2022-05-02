05114f359dae59a1af905e8fed4b5f44a8c66ad0
Herve Desrosiers <hdesrosiers@stingray.com>
2022-05-02 10:35:46 -0400

    click events

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import Home from "./Home";
 import Navbar from "./Navbar";
 
 function App() {
-  const likes = 50;
-
   return (
     <div className="App">
       <Navbar />
       <div className="content">
         <Home />
-        <p>Liked {likes} times</p>
       </div>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("Hello, ninjas", e);
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log("Hello " + name, e.target);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={(e) => handleClickAgain("dude", e)}>
+        Click Me Again
+      </button>
     </div>
   );
 };
