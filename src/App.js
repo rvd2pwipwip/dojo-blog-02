a52df34175f20ede874a5c8c870fc21f6d9523f0
Herve Desrosiers <hdesrosiers@stingray.com>
2022-05-02 09:10:42 -0400

    multiple components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
+import Home from "./Home";
+import Navbar from "./Navbar";
 
 function App() {
-  const title = "Welcome to the new blog";
   const likes = 50;
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
+        <Home />
         <p>Liked {likes} times</p>
       </div>
     </div>
