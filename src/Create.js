1cd244ed83c4992500c643c13e1af488fa25a5c2
Herve Desrosiers <hdesrosiers@stingray.com>
2022-05-05 09:31:34 -0400

    programmatic redirects

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
   const [isPending, setIsPending] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault(); //prevent page refresh
@@ -19,6 +21,7 @@ const Create = () => {
         body: JSON.stringify(blog),
       }).then(() => {
         setIsPending(false);
+        navigate("/");
       });
     }, 1000);
   };
